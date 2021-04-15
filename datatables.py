7ed5d2016f80f95dcea04477c35fd4b2c851b65f
nicsavit <nicsavit@umich.edu>
2021-04-15 14:22:36 -0400

    Nicole's inital api search

--- datatables.py
+++ datatables.py
@@ -1,1 +1,29 @@
+import json
+import requests
 
+#Historical Weather API
+
+# api_key = '777134bd57cb4678b1e144946211404'
+# location = "Honolulu, Hawaii"
+# date = '2015-01-21'
+# api_result = requests.get('http://api.worldweatheronline.com/premium/v1/past-weather.ashx'.format(api_key,location,date))
+# api_response = api_result.json()
+# print(api_response)
+
+
+#COVID STATES API
+
+# api_result_covid = requests.get('https://covidtracking.com/api/states')
+# api_response_covid = api_result_covid.json()
+# print(api_response_covid)
+
+#COVID DATA for Specific County
+
+
+# county= 'Washtenaw'
+# api_result_covid_county = requests.get('https://corona.lmao.ninja/v2/jhucsse/counties/'.format(county))
+# api_response_covid_county = api_result_covid_county.json()
+# print(api_response_covid_county)[:2]
+
+
+#This last one keeps crashing my code so maybe just states??
