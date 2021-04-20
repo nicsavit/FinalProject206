ab3ece4c690ce3d063a11eca703c82bc1227a7b3
Ellie Heller <ellieheller@synergy-gw.ip4.gtt.net>
2021-04-20 00:52:15 -0400

    implemented first visualization regarding covid data

--- datatables.py
+++ datatables.py
@@ -2,13 +2,8 @@ import json
 import requests
 import sqlite3
 import os
-
-# Weather API
-def get_county_weather(lat, lon, start_date, end_date):
-    api_result = requests.get(f'https://api.weatherbit.io/v2.0/history/daily?&lat={lat}&lon={lon}&start_date={start_date}&end_date={end_date}&key=ab0294577d37449cb2e5ed6fad640afa')
-    data = api_result.json()
-    print(data)
-    return data
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 #COVID DATA for Specific County
@@ -60,13 +55,73 @@ def setUpCovidCountyTable(cur, conn):
             cur.execute('INSERT INTO Covid (id, date, cases, deaths) VALUES (?,?,?,?)', (county_id, day['date'], int(day['cases']), int(day['deaths']))) 
            
      conn.commit()
+    
+def covid_visualization(cur, conn):
+    x = ["Washtenaw", "Cuyahoga", "Hennepin", "Broward"]
+    y = [0, 50, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750]
+    avg_covid_increase = []
 
+    county_0 = cur.execute('SELECT cases FROM Covid WHERE id = 0')
+    all_cases_for_county_0 = county_0.fetchall()
+    initial_case_value_for_county_0 = all_cases_for_county_0[0]
+    final_case_value_for_county_0 = all_cases_for_county_0[-1]
+    str_inital_0 = str(initial_case_value_for_county_0)
+    str_final_0 = str(final_case_value_for_county_0)
+    strip_inital_0 = str_inital_0.strip('(),')
+    strip_final_0 = str_final_0.strip('(),')
+    total_case_change_0 = int(strip_final_0) - int(strip_inital_0)
+    average_case_change_0 = total_case_change_0/len(all_cases_for_county_0)
+    avg_covid_increase.append(average_case_change_0)
+
+    county_1 = cur.execute('SELECT cases FROM Covid WHERE id = 1')
+    all_cases_for_county_1 = county_1.fetchall()
+    initial_case_value_for_county_1 = all_cases_for_county_1[0]
+    final_case_value_for_county_1 = all_cases_for_county_1[-1]
+    str_inital_1 = str(initial_case_value_for_county_1)
+    str_final_1 = str(final_case_value_for_county_1)
+    strip_inital_1 = str_inital_1.strip('(),')
+    strip_final_1 = str_final_1.strip('(),')
+    total_case_change_1 = int(strip_final_1) - int(strip_inital_1)
+    average_case_change_1 = total_case_change_1/len(all_cases_for_county_1)
+    avg_covid_increase.append(average_case_change_1)
+
+    county_2 = cur.execute('SELECT cases FROM Covid WHERE id = 2')
+    all_cases_for_county_2 = county_2.fetchall()
+    initial_case_value_for_county_2 = all_cases_for_county_2[0]
+    final_case_value_for_county_2 = all_cases_for_county_2[-1]
+    str_inital_2 = str(initial_case_value_for_county_2)
+    str_final_2 = str(final_case_value_for_county_2)
+    strip_inital_2 = str_inital_2.strip('(),')
+    strip_final_2 = str_final_2.strip('(),')
+    total_case_change_2 = int(strip_final_2) - int(strip_inital_2)
+    average_case_change_2 = total_case_change_2/len(all_cases_for_county_2)
+    avg_covid_increase.append(average_case_change_2)
+
+    county_3 = cur.execute('SELECT cases FROM Covid WHERE id = 3')
+    all_cases_for_county_3 = county_3.fetchall()
+    initial_case_value_for_county_3 = all_cases_for_county_3[0]
+    final_case_value_for_county_3 = all_cases_for_county_3[-1]
+    str_inital_3 = str(initial_case_value_for_county_3)
+    str_final_3 = str(final_case_value_for_county_3)
+    strip_inital_3 = str_inital_3.strip('(),')
+    strip_final_3 = str_final_3.strip('(),')
+    total_case_change_3 = int(strip_final_3) - int(strip_inital_3)
+    average_case_change_3 = total_case_change_3/len(all_cases_for_county_3)
+    avg_covid_increase.append(average_case_change_3)
+
+    print(avg_covid_increase)
+
+
+    y_pos = np.arange(len(avg_covid_increase))
+    plt.bar(y_pos, x, color = 'blue')
+    plt.xlabel("Counties")
+    plt.ylabel("Average Increase of Cases")
+    plt.title("Average Increase of Cases by County")
+    plt.xticks(y_pos, x)
+    plt.show()
 
 
 cur, conn = setUpDatabase('covid.db')
 setUpCountyTable(cur,conn)
 setUpCovidCountyTable(cur, conn)
-get_county_weather(43.308, 83.847, 2021-03-20, 2021-04-18)
-
-
-
+covid_visualization(cur, conn)
