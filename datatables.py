c9e7ca79ecd76de192433d15e676d06cfffb8491
nicsavit <nicsavit@umich.edu>
2021-04-21 11:27:08 -0400

    finished

--- datatables.py
+++ datatables.py
@@ -54,68 +54,58 @@ def setUpLocationTable(cur,conn):
 		"Minneapolis",
 		"Phoenix"
 	]
-	cur.execute("DROP TABLE IF EXISTS Locations")
-	cur.execute("CREATE TABLE Locations (id INTEGER PRIMARY KEY, state TEXT, county TEXT, subregion TEXT)")
+	#cur.execute("DROP TABLE IF EXISTS Locations")
+	cur.execute("CREATE TABLE IF NOT EXISTS Locations (id INTEGER PRIMARY KEY, state TEXT, county TEXT, subregion TEXT)")
 	for i in range(len(states)):
-		cur.execute("INSERT INTO Locations (id, state, county, subregion) VALUES (?,?,?,?)",(i,states[i], counties[i], subregions[i]))
-	conn.commit()
+		cur.execute("SELECT state FROM Locations WHERE state = ?", (states[i],))
+		if cur.fetchone() == None:
+			cur.execute("INSERT OR IGNORE INTO Locations (id, state, county, subregion) VALUES (?,?,?,?)",(i,states[i], counties[i], subregions[i]))
+			location_tup = (states[i], counties[i], subregions[i])
+			conn.commit()
+			return location_tup
+	
 
-def setUpCovidCountyTable(cur, conn):
-	cur.execute("DROP TABLE IF EXISTS Covid")
-	cur.execute("CREATE TABLE Covid (id INTEGER, date TEXT, cases INTEGER, deaths INTEGER)")
-	counties = [
-		"Washtenaw",
-		"Cuyahoga",
-		"Hennepin",
-		"Maricopa"
-	]
-	for c in counties:
-		data = get_countydata(c, 30)
+def setUpCovidCountyTable(county, cur, conn):
+	#cur.execute("DROP TABLE IF EXISTS Covid")
+	cur.execute("CREATE TABLE IF NOT EXISTS Covid (id INTEGER, date TEXT, cases INTEGER, deaths INTEGER)")
+	
+	
+	data = get_countydata(county, 25)
 
-		for day in data:
-			cur.execute('SELECT id FROM Locations WHERE county = ?', (day['county'],))
-			county_id = cur.fetchone()[0]
+	for day in data:
+		cur.execute('SELECT id FROM Locations WHERE county = ?', (day['county'],))
+		county_id = cur.fetchone()[0]
 
-			cur.execute('INSERT INTO Covid (id, date, cases, deaths) VALUES (?,?,?,?)', (county_id, day['date'], int(day['cases']), int(day['deaths']))) 
+		cur.execute('INSERT OR IGNORE INTO Covid (id, date, cases, deaths) VALUES (?,?,?,?)', (county_id, day['date'], int(day['cases']), int(day['deaths']))) 
 	
 	conn.commit()
 
-def setUpMobilityTable(cur,conn):
-	cur.execute("DROP TABLE IF EXISTS Mobility")
-	cur.execute("CREATE TABLE Mobility (id INTEGER, subregion TEXT, date TEXT, driving REAL, transit REAL, walking REAL)")
-	subregions = [
-		"Ann Arbor",
-		"Cleveland",
-		"Minneapolis",
-		"Phoenix"
-	]
+def setUpMobilityTable(subregion, cur,conn):
+	#cur.execute("DROP TABLE IF EXISTS Mobility")
+	cur.execute("CREATE TABLE IF NOT EXISTS Mobility (id INTEGER, subregion TEXT, date TEXT, driving REAL, transit REAL, walking REAL)")
+
 	
-	for s in subregions:
-		data1 = get_mobilitydata(s)
-		cur.execute('SELECT id FROM Locations WHERE subregion = ?', (s,))
-		subregion_id = cur.fetchone()[0]
-		for day in data1['data']:
-			#print(day)
-			cur.execute('INSERT INTO Mobility (id, subregion, date, driving, transit, walking) VALUES (?,?,?,?,?,?)', (subregion_id, day['subregion_and_city'],day['date'], float(day['driving']), float(day['transit']),float(day['walking']))) 
+	
+	data1 = get_mobilitydata(subregion)
+	cur.execute('SELECT id FROM Locations WHERE subregion = ?', (subregion,))
+	subregion_id = cur.fetchone()[0]
+	for day in data1['data']:
+		#print(day)
+		cur.execute('INSERT OR IGNORE INTO Mobility (id, subregion, date, driving, transit, walking) VALUES (?,?,?,?,?,?)', (subregion_id, day['subregion_and_city'],day['date'], float(day['driving']), float(day['transit']),float(day['walking']))) 
 	conn.commit()
 
 
-def setUpVaccineTable(cur,conn):
-	cur.execute("DROP TABLE IF EXISTS Vaccination")
-	cur.execute("CREATE TABLE Vaccination (id INTEGER, state TEXT, date TEXT, doses_admin INTEGER)")
-	states = [
-		"Michigan",
-		"Ohio",
-		"Minnesota",
-		"Arizona"
-	]
-	for s in states:
-		data = get_vaccinedata(s,30)
-		cur.execute('SELECT id FROM Locations WHERE state= ?', (s,))
-		state_id = cur.fetchone()[0]
-		for key in data['timeline']:
-			cur.execute('INSERT INTO Vaccination (id, state, date, doses_admin) VALUES (?,?,?,?)', (state_id, data['state'], key, int(data['timeline'][key])))
-		conn.commit()
+def setUpVaccineTable(state, cur,conn):
+	#cur.execute("DROP TABLE IF EXISTS Vaccination")
+	cur.execute("CREATE TABLE IF NOT EXISTS Vaccination (id INTEGER, state TEXT, date TEXT, doses_admin INTEGER)")
+	
+	
+	data = get_vaccinedata(state,25)
+	cur.execute('SELECT id FROM Locations WHERE state= ?', (state,))
+	state_id = cur.fetchone()[0]
+	for key in data['timeline']:
+		cur.execute('INSERT OR IGNORE INTO Vaccination (id, state, date, doses_admin) VALUES (?,?,?,?)', (state_id, data['state'], key, int(data['timeline'][key])))
+	conn.commit()
 def cases_and_vaccine_correlation(cur,conn):
 	cur.execute("SELECT Covid.cases, Vaccination.doses_admin FROM Covid LEFT JOIN Vaccination ON Covid.id = Vaccination.id")
 	corr = []
@@ -126,14 +116,16 @@ def cases_and_vaccine_correlation(cur,conn):
 		if tup[0] not in l:
 			l[tup[0]] = tup[1]
 	return l
+	
 
 
 
 
 cur, conn = setUpDatabase('covid.db')
-setUpLocationTable(cur,conn)
-setUpCovidCountyTable(cur, conn)
-setUpMobilityTable(cur,conn)
-
-setUpVaccineTable(cur,conn)
-# cases_and_vaccine_correlation(cur,conn)
+location_tup = setUpLocationTable(cur,conn)
+if location_tup:
+	(state, county, subregion) = location_tup
+	setUpCovidCountyTable(county,cur, conn)
+	setUpMobilityTable(subregion, cur,conn)
+	setUpVaccineTable(state, cur,conn)
+	cases_and_vaccine_correlation(cur,conn)
--- visuals.py
+++ visuals.py
@@ -4,6 +4,7 @@ import sqlite3
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 from datatables import *
 
 def covid_visualization(cur, conn):
@@ -201,11 +202,15 @@ def mobility_visualization(cur, conn):
 	plt.show()
 
 def covid_and_vaccine_visualization(cur, conn):
-    x_covid = ["Washtenaw", "Cuyahoga", "Hennepin", "Maricopa"]
+	#File Writing	
+	csvfile = open('covid_vaccine.csv', 'w')
+	write = csv.writer(csvfile)
+	write.writerow(['state', 'Average Covid Increase', 'Average Vaccine Increase'])
+	x_covid = ["Washtenaw", "Cuyahoga", "Hennepin", "Maricopa"]
 	y_covid = [0, 50, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750]
-    avg_covid_increase = []
+	avg_covid_increase = []
 
-    county_0 = cur.execute('SELECT cases FROM Covid WHERE id = 0')
+	county_0 = cur.execute('SELECT cases FROM Covid WHERE id = 0')
 	all_cases_for_county_0 = county_0.fetchall()
 	initial_case_value_for_county_0 = all_cases_for_county_0[0]
 	final_case_value_for_county_0 = all_cases_for_county_0[-1]
@@ -216,6 +221,7 @@ def covid_and_vaccine_visualization(cur, conn):
 	total_case_change_0 = int(strip_final_0) - int(strip_inital_0)
 	average_case_change_0 = total_case_change_0/len(all_cases_for_county_0)
 	avg_covid_increase.append(average_case_change_0)
+	
 
 	county_1 = cur.execute('SELECT cases FROM Covid WHERE id = 1')
 	all_cases_for_county_1 = county_1.fetchall()
@@ -270,7 +276,8 @@ def covid_and_vaccine_visualization(cur, conn):
 	strip_final_0 = str_final_0.strip('(),')
 	total_vaccine_change_0 = int(strip_final_0) - int(strip_inital_0)
 	average_vaccine_change_0 = total_vaccine_change_0/len(all_vaccines_for_state_0)
-	avg_vaccine_increase.append(average_vaccine_change_0)
+	avg_vaccine_increase.append(average_vaccine_change_0/100.0)
+	write.writerow(['Michigan',average_case_change_0, (average_vaccine_change_0/100.0)])
 
 	state_1 = cur.execute("SELECT doses_admin FROM Vaccination WHERE state = 'Ohio'")
 	all_vaccines_for_state_1 = state_1.fetchall()
@@ -282,7 +289,8 @@ def covid_and_vaccine_visualization(cur, conn):
 	strip_final_1 = str_final_1.strip('(),')
 	total_vaccine_change_1 = int(strip_final_1) - int(strip_inital_1)
 	average_vaccine_change_1 = total_vaccine_change_1/len(all_vaccines_for_state_1)
-	avg_vaccine_increase.append(average_vaccine_change_1)
+	avg_vaccine_increase.append(average_vaccine_change_1/100.0)
+	write.writerow(['Ohio',average_case_change_1, (average_vaccine_change_1/100.0)])
 
 	state_2 = cur.execute("SELECT doses_admin FROM Vaccination WHERE state = 'Minnesota'")
 	all_vaccines_for_state_2 = state_2.fetchall()
@@ -294,9 +302,10 @@ def covid_and_vaccine_visualization(cur, conn):
 	strip_final_2 = str_final_2.strip('(),')
 	total_vaccine_change_2 = int(strip_final_2) - int(strip_inital_2)
 	average_vaccine_change_2 = total_vaccine_change_2/len(all_vaccines_for_state_2)
-	avg_vaccine_increase.append(average_vaccine_change_2)
+	avg_vaccine_increase.append(average_vaccine_change_2/100.0)
+	write.writerow(['Minnesota',average_case_change_2, (average_vaccine_change_2/100.0)])
 
-	state_3 = cur.execute("SELECT doses_admin FROM Vaccination WHERE state = 'Minnesota'")
+	state_3 = cur.execute("SELECT doses_admin FROM Vaccination WHERE state = 'Arizona'")
 	all_vaccines_for_state_3 = state_3.fetchall()
 	initial_vaccine_value_for_state_3 = all_vaccines_for_state_3[0]
 	final_vaccine_value_for_state_3 = all_vaccines_for_state_3[-1]
@@ -306,29 +315,105 @@ def covid_and_vaccine_visualization(cur, conn):
 	strip_final_3 = str_final_3.strip('(),')
 	total_vaccine_change_3 = int(strip_final_3) - int(strip_inital_3)
 	average_vaccine_change_3 = total_vaccine_change_3/len(all_vaccines_for_state_3)
-	avg_vaccine_increase.append(average_vaccine_change_3)
-
+	avg_vaccine_increase.append(average_vaccine_change_3/100.0)
+	write.writerow(['Arizona',average_case_change_3, (average_vaccine_change_3/100.0)])
+	csvfile.close()
 	print(avg_vaccine_increase)
 
 
-    c = avg_covid_increase
-    v = avg_vaccine_increase
+	c = avg_covid_increase
+	v = avg_vaccine_increase
 
-    N = 4
-    y_pos = np.arange(N)
-    width = 0.35
+	N = 4
+	y_pos = np.arange(N)
+	width = 0.35
+	# fig = plt.figure()
+	# ax = fig.add_axes([0,0,1,1])
+	fig, ax = plt.subplots()
+	x = np.arange(len(x_vaccine))
+	p1 = ax.bar(y_pos, c, width, label='Average Covid Increase')
+	p2 = ax.bar(y_pos + width, v, width, label='Average Vaccine Increase')
+	ax.set_ylabel('Average Increase of Vaccines and Covid Cases Per Day')
+	ax.set_title('Average Increase of Vaccines and Covid Cases Per Day by Location')
+	ax.set_xticks(x)
+	ax.set_xticklabels(x_vaccine)
+	ax.legend(loc = 'best')
+	ax.autoscale_view()
+	plt.show()
 
-    plt.bar(ind, men_means, width, label='Average Covid Increase')
-    plt.bar(ind + width, women_means, width,
-    label='Average Vaccine Increase')
-    plt.ylabel('Average Increase of Vaccines and Covid Cases Per Day')
-    plt.title('Average Increase of Vaccines and Covid Cases Per Day by Location')
-	plt.xticks(y_pos + width / 2, ([0,1,2,3], x))
-    plt.legend(loc='best')
-    plt.show()
+	
+
+
+
+
+def mobility_visualization_2(cur, conn):
+	x = ["Ann Arbor", "Cleveland", "Minneapolis", "Phoenix"]
+	y = [0, 50, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750]
+	avg_driving_increase = []
+
+	city_0 = cur.execute('SELECT driving FROM Mobility WHERE subregion = "Ann Arbor"')
+	all_driving_for_city_0 = city_0.fetchall()
+	initial_driving_value_for_city_0 = all_driving_for_city_0[0]
+	final_driving_value_for_city_0 = all_driving_for_city_0[-1]
+	str_inital_0 = str(initial_driving_value_for_city_0)
+	str_final_0 = str(final_driving_value_for_city_0)
+	strip_inital_0 = str_inital_0.strip('(),')
+	strip_final_0 = str_final_0.strip('(),')
+	total_driving_change_0 = float(strip_final_0) - float(strip_inital_0)
+	average_driving_change_0 = total_driving_change_0/len(all_driving_for_city_0)
+	avg_driving_increase.append(average_driving_change_0)
+
+	city_1 = cur.execute('SELECT driving FROM Mobility WHERE subregion = "Cleveland"')
+	all_driving_for_city_1 = city_1.fetchall()
+	initial_driving_value_for_city_1 = all_driving_for_city_1[0]
+	final_driving_value_for_city_1 = all_driving_for_city_1[-1]
+	str_inital_1 = str(initial_driving_value_for_city_1)
+	str_final_1 = str(final_driving_value_for_city_1)
+	strip_inital_1 = str_inital_1.strip('(),')
+	strip_final_1 = str_final_1.strip('(),')
+	total_driving_change_1 = float(strip_final_1) - float(strip_inital_1)
+	average_driving_change_1 = total_driving_change_1/len(all_driving_for_city_1)
+	avg_driving_increase.append(average_driving_change_1)
+
+	city_2 = cur.execute('SELECT driving FROM Mobility WHERE subregion = "Minneapolis"')
+	all_driving_for_city_2 = city_2.fetchall()
+	initial_driving_value_for_city_2 = all_driving_for_city_2[0]
+	final_driving_value_for_city_2 = all_driving_for_city_2[-1]
+	str_inital_2 = str(initial_driving_value_for_city_2)
+	str_final_2 = str(final_driving_value_for_city_2)
+	strip_inital_2 = str_inital_2.strip('(),')
+	strip_final_2 = str_final_2.strip('(),')
+	total_driving_change_2 = float(strip_final_2) - float(strip_inital_2)
+	average_driving_change_2 = total_driving_change_2/len(all_driving_for_city_2)
+	avg_driving_increase.append(average_driving_change_2)
+
+	city_3 = cur.execute('SELECT driving FROM Mobility WHERE subregion = "Phoenix"')
+	all_driving_for_city_3 = city_3.fetchall()
+	initial_driving_value_for_city_3 = all_driving_for_city_3[0]
+	final_driving_value_for_city_3 = all_driving_for_city_3[-1]
+	str_inital_3 = str(initial_driving_value_for_city_3)
+	str_final_3 = str(final_driving_value_for_city_3)
+	strip_inital_3 = str_inital_3.strip('(),')
+	strip_final_3 = str_final_3.strip('(),')
+	total_driving_change_3 = float(strip_final_3) - float(strip_inital_3)
+	average_driving_change_3 = total_driving_change_3/len(all_driving_for_city_3)
+	avg_driving_increase.append(average_driving_change_3)
+
+	print(avg_driving_increase)
+
+
+	y_pos = np.arange(len(avg_driving_increase))
+	plt.bar(y_pos, avg_driving_increase, color = 'purple')
+	plt.xlabel("Counties")
+	plt.ylabel("Average Change in Driving")
+	plt.title("Average Change in Driving by City")
+	plt.xticks([0,1,2,3], x)
+	plt.show()
 
 cur, conn = setUpDatabase('covid.db')
 covid_visualization(cur, conn)
 vaccine_visualization(cur, conn)
 mobility_visualization(cur, conn)
 covid_and_vaccine_visualization(cur, conn)
+mobility_visualization_2(cur, conn)
+
