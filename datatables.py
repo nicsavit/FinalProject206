6e5bf2370defe03282f9c60fad64754c07a14287
Ellie Heller <ellieheller@synergy-gw.ip4.gtt.net>
2021-04-20 20:47:10 -0400

    attempted second visualization

--- datatables.py
+++ datatables.py
@@ -168,10 +168,78 @@ def covid_visualization(cur, conn):
 	plt.show()
 
 
+def vaccine_visualization(cur, conn):
+	# increase of covid cases per day. average change in walking percentage
+	x = ["Michigan", "Ohio", "Minnesota", "Arizona"]
+	y = [0, 50, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750]
+	avg_vaccine_increase = []
+
+	state_0 = cur.execute('SELECT doses_admin FROM Vaccination WHERE state = Michigan')
+	all_vaccines_for_state_0 = state_0.fetchall()
+	initial_vaccine_value_for_state_0 = all_vaccines_for_state_0[0]
+	final_vaccine_value_for_state_0 = all_vaccines_for_state_0[-1]
+	str_inital_0 = str(initial_vaccine_value_for_state_0)
+	str_final_0 = str(final_vaccine_value_for_state_0)
+	strip_inital_0 = str_inital_0.strip('(),')
+	strip_final_0 = str_final_0.strip('(),')
+	total_vaccine_change_0 = int(strip_final_0) - int(strip_inital_0)
+	average_vaccine_change_0 = total_vaccine_change_0/len(all_vaccines_for_state_0)
+	avg_vaccine_increase.append(average_vaccine_change_0)
+
+	state_1 = cur.execute('SELECT doses_admin FROM Vaccination WHERE state = Ohio')
+	all_vaccines_for_state_1 = state_1.fetchall()
+	initial_vaccine_value_for_state_1 = all_vaccines_for_state_1[0]
+	final_vaccine_value_for_state_1 = all_vaccines_for_state_1[-1]
+	str_inital_1 = str(initial_vaccine_value_for_state_1)
+	str_final_1 = str(final_vaccine_value_for_state_1)
+	strip_inital_1 = str_inital_1.strip('(),')
+	strip_final_1 = str_final_1.strip('(),')
+	total_vaccine_change_1 = int(strip_final_1) - int(strip_inital_1)
+	average_vaccine_change_1 = total_vaccine_change_1/len(all_vaccines_for_state_1)
+	avg_vaccine_increase.append(average_vaccine_change_1)
+
+	state_2 = cur.execute('SELECT doses_admin FROM Vaccination WHERE state = Minnesota')
+	all_vaccines_for_state_2 = state_2.fetchall()
+	initial_vaccine_value_for_state_2 = all_vaccines_for_state_2[0]
+	final_vaccine_value_for_state_2 = all_vaccines_for_state_2[-1]
+	str_inital_2 = str(initial_vaccine_value_for_state_2)
+	str_final_2 = str(final_vaccine_value_for_state_2)
+	strip_inital_2 = str_inital_2.strip('(),')
+	strip_final_2 = str_final_2.strip('(),')
+	total_vaccine_change_2 = int(strip_final_2) - int(strip_inital_2)
+	average_vaccine_change_2 = total_vaccine_change_2/len(all_vaccines_for_state_2)
+	avg_vaccine_increase.append(average_vaccine_change_2)
+
+	state_3 = cur.execute('SELECT doses_admin FROM Vaccination WHERE state = Minnesota')
+	all_vaccines_for_state_3 = state_3.fetchall()
+	initial_vaccine_value_for_state_3 = all_vaccines_for_state_3[0]
+	final_vaccine_value_for_state_3 = all_vaccines_for_state_3[-1]
+	str_inital_3 = str(initial_vaccine_value_for_state_3)
+	str_final_3 = str(final_vaccine_value_for_state_3)
+	strip_inital_3 = str_inital_3.strip('(),')
+	strip_final_3 = str_final_3.strip('(),')
+	total_vaccine_change_3 = int(strip_final_3) - int(strip_inital_3)
+	average_vaccine_change_3 = total_vaccine_change_3/len(all_vaccines_for_state_3)
+	avg_vaccine_increase.append(average_vaccine_change_3)
+
+	print(avg_vaccine_increase)
+
+
+	y_pos = np.arange(len(avg_vaccine_increase))
+	plt.bar(y_pos, avg_vaccine_increase, color = 'green')
+	plt.xlabel("States")
+	plt.ylabel("Average Increase of Vaccines Per Day")
+	plt.title("Average Increase of Vaccines Per Day by State")
+	plt.xticks([0,1,2,3], x)
+	plt.show()
+
+
+
 cur, conn = setUpDatabase('covid.db')
 setUpCountyTable(cur,conn)
 setUpCovidCountyTable(cur, conn)
 setUpMobilityTable(cur,conn)
-#covid_visualization(cur, conn)
+covid_visualization(cur, conn)
 get_vaccinedata('Arizona', 30)
 setUpVaccineTable(cur,conn)
+vaccine_visualization(cur, conn)
